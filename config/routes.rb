fa02fe98cc2f575cc1ebf8e924ecdda3280d5aae
Lars Åge Kamfjord <lars.age@kamfjord.org>
2022-02-05 17:08:23 +0100

    Day 1 work manual and automatic posts and articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts
+  get "articles", to: "articles#index"
+  get "articles/new", to: "articles#new"
+  post "articles", to: "articles#create"
+  #get "/articles/:id", to: "articles#show", as: "article"
+  get "/articles/:id", to: "articles#show", as: "article"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_02_05_131014) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
